62473bf547c2f3e1e1847b630b04efe9ac9da19a
JoesephEgbenchong <egbenchongjosephayuk@gmail.com>
2023-05-26 22:36:48 +0100

    Started and finished home page. Started the scanning page..

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,36 @@
 import React from 'react'
+import logo from '../assets/Medx_logo.png'
+import welcomeImage from '../assets/welcome_image.png';
+import { Link } from 'react-router-dom';
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <>
+        <div className='max-w-7xl ml-auto flex justify-between'>
+            <div className='w-full'>
+                <img src={logo} alt="MEDx-logo" className='mt-[20px]' />
+                <h1 className='mt-[200px] text-[44px] leading-tight font-bold uppercase text-dark font-Axiforma'>
+                    keeping communities safe, one scan at a time</h1>
+                <p className='font-medium text-xl font-Axiforma text-brandGreen 
+                mb-[80px]'>Let’s ensure a secure environment with QR code verification</p>
+
+                <div>
+                    <Link to="/new-scan" className='text-white bg-dark px-7 py-4 border-2 border-dark
+                    text-2xl shadow-md font-Axiforma font-medium rounded-[10px] leading-[1.5] cursor-pointer 
+                    w-full hover:border-2 transition duration-150 ease-in-out hover:border-dark 
+                    hover:text-dark hover:bg-white hover:shadow-lg'>Get Started</Link>
+
+                    <Link to="/" className='text-dark bg-white px-7 py-4 ml-[40px] 
+                    text-2xl shadow-md font-Axiforma font-medium rounded-[10px] leading-[1.5] cursor-pointer 
+                    w-full border-2 transition duration-150 ease-in-out border-dark 
+                    hover:text-white hover:bg-dark hover:shadow-lg'>Contact Us</Link>
+                </div>
+            </div>
+            <div className='w-[100%] ml-[30px]'>
+                <img src={welcomeImage} alt="Welcome Image" height="500"/>
+            </div>
+            
+        </div>
+    </>
   )
 }
--- src/pages/Scan.jsx
+++ src/pages/Scan.jsx
@@ -1,7 +1,39 @@
 import React from 'react'
+import logo from '../assets/Medx_logo.png'
+import { Link } from 'react-router-dom'
+import { BsChevronLeft, BsChevronDown } from 'react-icons/bs'
 
 export default function Scan() {
   return (
-    <div>Scan</div>
+    <>
+      <div className='max-w-7xl mx-auto flex justify-between'>
+        <div className='w-full'>
+          <img src={logo} alt="MEDx-logo" className='mt-[20px] mb-[200px]' />
+
+          <h2 className='font-bold text-[50px] leading-[1.44] text-dark font-Axiforma'>Scan to Verify your <br/> COVID-19 Vaccination</h2>
+          <p className='text-2xl font-normal text-dark font-Axiforma'>Place your QR code within the frame</p>
+          <p className='mt-[400px] mb-2 text-sm text-dark font-Axiforma font-medium'>Copyright© 2023 MEDx eHealthCenter B.V. All Rights Reserved.</p>
+        </div>
+        <div className='w-full ml-[30px]'>
+          <div className='mt-9 text-right flex items-center justify-end'>
+            <Link to="/" className='text-lg font-medium text-dark font-Axiforma flex items-center gap-2 
+            hover:text-brandGreen'> 
+              <BsChevronLeft className='text-lg'/>Home
+            </Link>
+          </div>
+
+          <div className='w-full mt-[30px] flex items-center justify-between'>
+            <button className='px-3 py-2 bg-[#d9d8d8] rounded-[10px] text-dark shadow-md 
+            font-medium text-lg font-Axiforma flex items-center gap-4' disabled>
+              Select Camera <BsChevronDown className='text-lg'/>
+            </button>
+
+            <button className='px-3 py-2 rounded-[10px] bg-[#5c61d7] text-white shadow-md 
+            font-medium text-lg font-Axiforma hover:bg-[#494ca7] transition duration-150 ease-in-out 
+            hover:shadow-lg active:bg-[#3e408d] active:shadow-xl'>Activate Camera</button>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
